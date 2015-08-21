5ff16c1f855d8d6c6c0e29d0d2866dde3b0eb90d
Mareike <mareikep@tzi.de>
2015-08-21 10:48:34 +0200

    refactoring & cleanup of webln

--- webrob/pages/routes.py
+++ webrob/pages/routes.py
@@ -1,4 +1,4 @@
-from webmln.mlninit import mlnApp
+from webmln.gui.app import mlnApp
 import os
 from os.path import expanduser
 import jinja2
@@ -13,20 +13,18 @@ def register_routes():
     # use html templates from mln app
     mln_loader = jinja2.ChoiceLoader([
         mlnApp.app.jinja_loader,
-        jinja2.FileSystemLoader([
-            '/opt/practools/tools/pracmln/mlnGUI/webmln/templates']),
+        jinja2.FileSystemLoader(['/opt/practools/tools/pracmln/webmln/gui/templates']),
     ])
     mlnApp.app.jinja_loader = mln_loader
     mlnApp.app.secret_key = 'so secret!'
-    mlnApp.app.config['MLN_STATIC_PATH'] = \
-        '/opt/practools/tools/pracmln/mlngui/webmln/build'
+    mlnApp.app.config['MLN_STATIC_PATH'] = '/opt/practools/tools/pracmln/webmln/gui/build'
 
     # settings for fileuploads and logging
     home = expanduser("~")
     mlnApp.app.config['ALLOWED_EXTENSIONS'] = set(['mln','db','pracmln','emln'])
     mlnApp.app.config['UPLOAD_FOLDER'] = os.path.join(home, 'mlnfiles')
-    mlnApp.app.config['EXAMPLES_FOLDER'] = '/opt/practools/tools/pracmln/examples'
     mlnApp.app.config['MLN_ROOT_PATH'] = '/opt/practools/tools/pracmln'
+    mlnApp.app.config['EXAMPLES_FOLDER'] = os.path.join(mlnApp.app.config['MLN_ROOT_PATH'], 'examples')
     mlnApp.app.config['LOG_FOLDER'] = os.path.join(mlnApp.app.config['UPLOAD_FOLDER'], 'log')
 
     if not os.path.exists(mlnApp.app.config['UPLOAD_FOLDER']):
@@ -44,8 +42,8 @@ def register_routes():
 
 
     from webrob.pages import log
-    from webmln.pages import learning
-    from webmln.pages import inference
-    from webmln.pages import views
-    from webmln.pages import fileupload
-    from webmln.pages import utils
+    from webmln.gui.pages import learning
+    from webmln.gui.pages import inference
+    from webmln.gui.pages import views
+    from webmln.gui.pages import fileupload
+    from webmln.gui.pages import utils
